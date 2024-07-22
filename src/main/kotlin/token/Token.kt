5d18c97042d135704908042fd31f43d144c4ed22
edu <bfwaend@gmail.com>
2024-07-22 13:29:08 -0300

    Improve scanning

--- src/main/kotlin/token/Token.kt
+++ src/main/kotlin/token/Token.kt
@@ -3,7 +3,7 @@ package ft.etachott.tokens
 class Token(
     val type: TokenType,
     val lexeme: String,
-    val literal: Any,
+    val literal: Any?,
     val tokenLine: Int,
 ) {
     override fun toString(): String = "$type $lexeme $literal"
