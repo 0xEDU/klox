5efc6e18028b0110bd857dae6d3c925012e061c3
edu <bfwaend@gmail.com>
2024-07-25 15:48:32 -0300

    Improve generator script and generated class

--- src/main/kotlin/expression/Expr.kt
+++ src/main/kotlin/expression/Expr.kt
@@ -3,19 +3,35 @@ package ft.etachott.expression
 import ft.etachott.tokens.Token
 
 sealed class Expr {
+    interface Visitor<R> {
+        fun visitBinaryExpr(expr: Binary): R
+        fun visitGroupingExpr(expr: Grouping): R
+        fun visitLiteralExpr(expr: Literal): R
+        fun visitUnaryExpr(expr: Unary): R
+    }
     data class Binary (
         val left: Expr?,
         val operator: Token,
         val right: Expr?,
-    )
+    ) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R = visitor.visitBinaryExpr(this) 
+    }
     data class Grouping (
         val expression: Expr?,
-    )
+    ) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R = visitor.visitGroupingExpr(this) 
+    }
     data class Literal (
         val value: Any?,
-    )
+    ) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R = visitor.visitLiteralExpr(this) 
+    }
     data class Unary (
         val operator: Token,
         val right: Expr?,
-    )
+    ) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R = visitor.visitUnaryExpr(this) 
+    }
+
+    abstract fun <R> accept(visitor: Visitor<R>): R
 }
--- utils/ast_generator.py
+++ utils/ast_generator.py
@@ -1,12 +1,26 @@
 #!/bin/python3
 import sys
 
+FOUR_SPACES = f"{'':4}"
+EIGHT_SPACES = f"{'':8}"
+TWELVE_SPACES = f"{'':12}"
+
+
+def define_visitor(stream, base_name, types):
+    stream.write(f"{FOUR_SPACES}interface Visitor<R> {{\n")
+    for type_name in types.keys():
+        stream.write(f"{EIGHT_SPACES}fun visit{type_name}{base_name}({base_name.lower()}: {type_name}): R\n")
+    stream.write(f"{FOUR_SPACES}}}\n")
+    pass
+
 
 def define_type(stream, base_name, class_name, field_list):
-    stream.write(f"{'':<4}data class {class_name} (\n")
+    stream.write(f"{FOUR_SPACES}data class {class_name} (\n")
     for field in field_list:
-        stream.write(f"{'':<8}val {field},\n")
-    stream.write(f"{'':<4})\n")
+        stream.write(f"{EIGHT_SPACES}val {field},\n")
+    stream.write(f"{FOUR_SPACES}) : {base_name}() {{\n")
+    stream.write(f"{EIGHT_SPACES}override fun <R> accept(visitor: Visitor<R>): R = visitor.visit{class_name}{base_name}(this) \n")
+    stream.write(f"{FOUR_SPACES}}}\n")
 
 
 def define_ast(output_dir, base_name, types):
@@ -15,8 +29,10 @@ def define_ast(output_dir, base_name, types):
         stream.write("package ft.etachott.expression\n\n")
         stream.write("import ft.etachott.tokens.Token\n\n")
         stream.write(f"sealed class {base_name} {{\n")
+        define_visitor(stream, base_name, types)
         for k, v in types.items():
             define_type(stream, base_name, k, v)
+        stream.write(f"\n{FOUR_SPACES}abstract fun <R> accept(visitor: Visitor<R>): R\n")
         stream.write("}")
 
 
